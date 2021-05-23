2766b739f8ddca376808f1719bb12aae09772149
hadizazz <hadizazz81@gmail.com>
2021-05-23 17:32:15 +0700

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { CarouselData } from './components/CarouselData';
+import CarouselImg from './components/CarouselImg';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <CarouselImg slides={CarouselData}/>
     </div>
   );
 }
---
+++ src/components/CarouselData.js
@@ -0,0 +1,23 @@
+export const CarouselData = [
+    {
+        image:'https://wallpapercave.com/wp/wp4190861.jpg'
+    },
+    {
+        image:'https://c4.wallpaperflare.com/wallpaper/308/441/192/tanah-lot-is-a-rock-near-the-indonesian-island-bali-temple-pura-tanah-lot-photography-2560%C3%971440-wallpaper-preview.jpg'
+    },
+    {
+        image:'https://www.celebes.co/wp-content/uploads/2019/10/Taman-Nasional-Wakatobi-Keindahan-Bawah-Laut-Sulawesi.jpg'
+    },
+    {
+        image:'https://lh3.googleusercontent.com/proxy/KJrExR9YKatGTS-E5okNDTSUt4iIi-z04J3syYv8bbYe3E1qBkMYYKJLH9U2HOF0waGzpc3Z3HdOKzN9qZK-vxLdkSjYmWIAsSou12araJVIZdEhwpurDTsJis2aa6qaGfvn1TSqzzFXh1-JYvYCmik-bvmhStoavnQR'
+    },
+    {
+        image:'https://pinthar.net/wp-content/uploads/2020/07/kebun-teh-rancabali-1.jpg'
+    },
+    {
+        image:'http://goborobudur.com/wp-content/uploads/2016/02/150911.1349291.jpg'
+    },
+    {
+        image:'https://www.hargatiket.net/wp-content/uploads/2018/12/Harga-Tiket-Masuk-Kawah-Ijen.jpg'
+    }
+]
